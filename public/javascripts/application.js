6e8991ede355817fc6bef0ba425f71474601e057
Rodrigo Yrigoyen Gonzales <rodry_yg@hotmail.com>
2014-08-06 18:09:04 -0500

    Permitir scroll automático para el panel de controles de lado

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,8 +1,15 @@
 /*
- * Función para corregir el scroll horizontal para 'main aside' y 'header'
+ * Corregir el scroll horizontal para 'main aside' y 'header'
  */
-$(window).on('scroll',function() {
+$(window).on('scroll', function() {
 	$('header, main aside').css({
 		'margin-left': $(this).scrollLeft() * -1
 	});
 });
+
+/*
+ * Hacer que 'aside#controls' llene el espacio entre header y el final de la ventana
+ */
+$(window).on('load resize', function() {
+	$('aside#controls').css('height', $(this).height() - $('header').height());
+})
