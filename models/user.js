3bb24f1c5705aa6828f78f5626f49de59c49677f
Diego Camino Reinoso <diego.camino.r@gmail.com>
2016-03-31 15:56:05 -0500

    Creación de schema Action y Tag

--- models/user.js
+++ models/user.js
@@ -6,11 +6,30 @@ var UserSchema = new Schema({
   provider: String,
   providerId: {type: String, unique: true},
   photo: String,
-  createdAt: {type: Date, default: Date.now}
+  createdAt: {type: Date, default: Date.now},
+  blockedUsers : [{ type: Schema.Types.ObjectId, ref: 'User' }]
 });
+
+var ActionSchema = new Schema({
+  name: String,
+  location: String,
+  photo: String,
+  createdAt: {type: Date, default: Date.now},
+  author : [{ type: Schema.Types.ObjectId, ref: 'User' }]
+});
+
+var TagSchema = new Schema({
+  name: String,
+  eventName : String,
+  createdAt: {type: Date, default: Date.now},
+  actions : [{ type: Schema.Types.ObjectId, ref: 'Action' }]
+});
+
 
 UserSchema.methods.signUp = function signUp () {
 		
 }
 
 var User = mongoose.model('User', UserSchema);
+var Action = mongoose.model('Action', ActionSchema);
+var Tag = mongoose.model('Tag', TagSchema);
