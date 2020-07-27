3e057830362d24867b9cfdf2142309a6da4b4070
Diego Camino Reinoso <diego.camino.r@gmail.com>
2020-07-26 23:38:01 -0500

    Refactorización login

---
+++ config/index.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+
+const config = {
+  dev: process.env.NODE_ENV !== "production",
+  port: process.env.PORT || 3000,
+  cors: process.env.CORS,
+  dbUser: process.env.DB_USER,
+  dbPassword: process.env.DB_PASSWORD,
+  dbHost: process.env.DB_HOST,
+  dbName: process.env.DB_NAME,
+  authJwtSecret: process.env.AUTH_JWT_SECRET,
+  facebookClientId: process.env.FACEBOOK_CLIENT_ID,
+  facebookClientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+};
+
+module.exports = { config };
